051ae1d9fd563f2a4dfd6f27de65a6a632dc593b
97hyun97 <coramel1404@gmail.com>
2025-11-12 17:50:58 +0900

    0.1.3 : ERC20

--- 04WordsERC20.sol
+++ 04WordsERC20.sol
@@ -3,8 +3,124 @@ pragma solidity 0.8.30;
 
 
 //혼자 짜봄
-contract ERC20practice {
+contract WordsonChainERC20 {
+
+    string TokenName = "Words on Chain Token";
+    string TokenSymbol = "Words";
+    uint8 TokenDecimals = 18;
+    uint TokenTotalSupply = 1000000000; // 1B total supply
+
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+
+    function name() public view returns (string memory _name) {
+        // In Solidity, when returning a reference type (like string, array, struct, etc.) 
+        // from a function, you must explicitly specify its data location (memory or calldata).
+        // 어디에 저장할지 명시해야 -> 가스비를 의도한 대로 쓸 수 있음.
+        return TokenName;
+    }
+
+    function symbol() public view returns (string memory) {
+        return TokenSymbol;
+    }
+
+    function decimals () public view returns (uint8) {
+        return TokenDecimals;
+    }
+
+    function totalSupply() public view returns (uint) {
+        return TokenTotalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint balance) {
+        return balances[_owner];
+    }
+
+    function transfer(address _to, uint _amount) public returns (bool success) {
+        
+        require(balances[msg.sender] >= _amount); //체크
+        
+        balances[msg.sender] -= _amount; //차감
+
+        balances[_to] += _amount; //전송
+        
+        emit Transfer(msg.sender, _to, _amount); //이벤트
+        
+        return true; //함수가 끝까지 실행됐다 = 전송이 잘 됐다 = 성공 반환
+    }
+
     
+
+    function approve(address _spender, uint _amount) public returns (bool success) {
+        
+        require(_spender != address(0), "You should not allow to zero address"); // 소각주소에 allow하는 거 아닌지 확인.
+        
+        allowance[msg.sender][_spender] = _amount;
+        // mapping(address => mapping(address => uint)) public allowance; 구조임. 따라서 mapping을 이어서 위와 같이 사용.
+
+        // ERC20의 기본 코드는 allowance[msg.sender][_spender] = _amount;
+        // 이는 front-run Attack에 취약.
+            // allowance 100 설정 -> allowance 50 덮어쓰기-를 할 때 mempool에서 이를 읽은 spender가 악의를 품고 100을 출금 -> 100 출금이 먼저 실행되고 50 덮어쓰기
+            // -> 결과적으로 owner는 allowance를 50으로 수정하고 싶었으나, 100 출금+50 allowance = 150 이 탈취됨.
+        // 이는 ERC-20 자체의 취약점.
+        // OpenZeppelin에서는 다음과 같이 보안.
+            // UI 단에서 기존 allowance값을 먼저 0으로 만드는 TX를 보내고, 그다음 새 값을 설정하는 TX를 보내도록 유도.(단점: 가스비 중복)
+            // increaseAllowance, decreaseAllowance 추가 권장함.
+            // 그런데 이렇게 해도 결국 CA 단에서 front-run attack를 막을 순 없고, UI 단에서 어떤 함수 호출할지 잘 정해야 되는 거임.
+        
+        emit Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
+
+        return true;
+    }
+
+    function increaseAllowance(address _spender, uint _increaseamount) public returns (bool success) {
+        
+        uint currentAllowance = allowance[msg.sender][_spender];
+        
+        allowance[msg.sender][_spender] = currentAllowance - _increaseamount;
+
+        emit Approval(msg.sender, _spender, currentAllowance);
+
+        return true;
+    }
+
+    function decreaseAllowance(address _spender, uint _decreaseamount) public returns (bool success) {
+
+        uint currentAllowance = allowance[msg.sender][_spender]; //매번 호출하지 말고 처음에 memory에 저장. -> 가스비 절약
+
+        require(currentAllowance >= _decreaseamount, "allowance must be bigger than _decreaseamount");
+        
+        allowance[msg.sender][_spender] = currentAllowance - _decreaseamount;
+
+        emit Approval(msg.sender, _spender, currentAllowance);
+
+        return true;
+    }
+
+
+    function transferFrom(address _from, address _to, uint _amount) public returns (bool success) {
+
+        uint currentAllowance = allowance[_from][msg.sender];
+
+        require(currentAllowance >= _amount, "_amount must be same or smaller than allowance."); // allowance 체크
+
+        require(balances[_from] >= _amount, "don't have enough amount."); // 밸런스 체크
+
+        allowance[_from][msg.sender] = currentAllowance - _amount; // allowance 차감
+
+        balances[_from] -= _amount; // 차감
+
+        balances[_to] += _amount; // 전송
+
+        emit Transfer(_from, _to, _amount); //이벤트
+
+        return true;
+
+    }
+
 }
 
 
